e76ef9ef0aae9ac2b72f4d77ed38ac1c527255a3
Nathalie Yap <nyap1@students.towson.edu>
2021-04-20 13:15:26 -0400

    added signin/up pages

---
+++ public/signin.js
@@ -0,0 +1,39 @@
+
+//signup
+const signupBtn = document.querySelector('#signup-btn');
+    signupBtn.addEventListener('click', e=>{
+        e.preventDefault();
+
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+
+        auth.createUserWithEmailAndPassword(email, password).then(cred => {
+            console.log('User signed up!');
+        });
+    });
+
+//sign in
+const loginBtn = document.querySelector('#login-btn');
+    loginBtn.addEventListener('click', e => {
+        e.preventDefault();
+  
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+  
+        auth.signInWithEmailAndPassword(email, password)
+        .then(cred => {
+            console.log('Logged in user!');
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
+  });
+
+//sign in state
+auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log(user.email + " is logged in!");
+    } else {
+      console.log('User is logged out!');
+    }
+  });
