3919f63a7c43389fc01e6fc1428a3f996637af67
Nathalie Yap <nyap1@students.towson.edu>
2021-04-26 20:20:27 -0400

    fixed firebase errors + added separate scripts

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+var firebaseConfig = {
+  apiKey: "AIzaSyCsjzeGbTVxJwMlO5qJkRiIO9xTB_sM3_k",
+  authDomain: "apcp-generator.firebaseapp.com",
+  databaseURL: "https://acpc-generator.firebaseio.com",
+  projectId: "apcp-generator",
+  storageBucket: "apcp-generator.appspot.com",
+  messagingSenderId: "715065907857",
+  appId: "1:715065907857:web:565c06a9eef5b2cd0ffd30",
+  measurementId: "G-P5GKQLBGCT"
+};
 
-    var modals = document.querySelectorAll('.modal');
-    M.Modal.init(modals);
-  
-    var items = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(items);
-  
-  });
+try {
+    firebase.initializeApp(firebaseConfig);
+  } catch (err) {
+    if (!/already exists/.test(err.message)) {
+      console.error('Firebase initialization error', err.stack);
+    }
+  }
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+db.settings({timestampsInSnapshots:true});
